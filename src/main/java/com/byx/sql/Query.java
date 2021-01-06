85233b3d0d259b24b1c9e3a39d66f1a10dd1fd9d
byx2000 <416982860@qq.com>
2021-01-06 21:34:03 +0800

    添加SQL语法树结构层次

---
+++ src/main/java/com/byx/sql/Query.java
@@ -0,0 +1,17 @@
+package com.byx.sql;
+
+import java.util.List;
+
+public final class Query implements SqlComponent
+{
+    private final List<SelectItem> select;
+    private final FromItem from;
+    private final Condition condition;
+
+    public Query(List<SelectItem> select, FromItem from, Condition condition)
+    {
+        this.select = select;
+        this.from = from;
+        this.condition = condition;
+    }
+}
